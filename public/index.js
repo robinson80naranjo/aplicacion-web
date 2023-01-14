2627502e06400b21334fc1284827ce061bec96da
unknown <robinsonnaranjo700@gmail.com>
2023-01-13 21:07:31 -0500

    Actualizacion

--- public/index.js
+++ public/index.js
@@ -1,1 +1,11 @@
-console.log("Hola");
+const cont_emergente = document.querySelector(".cont_emergente");
+const buscar = document.querySelector(".buscar");
+const regresar = document.querySelector(".regresar");
+
+buscar.addEventListener("click", ()=>{
+	cont_emergente.style.display = "flex";
+});
+
+regresar.addEventListener("click", ()=>{
+	cont_emergente.style.display = "none";
+});
